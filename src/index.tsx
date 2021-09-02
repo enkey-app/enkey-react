3da9bf18e29253d6bde64a27b86e76be74092055
Casey Collins <kcc989@gmail.com>
2021-09-01 22:29:59 -0400

    husky?

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SayHello = ({ name, other = true }: { name: string; other?: boolean }): JSX.Element => (
+const SayHello = ({ name, other = true, c }: { name: string; other?: boolean }): JSX.Element => (
     <div>
         Hey {name}, say {other ? 'hello' : 'goodbye'} to TypeScript.
     </div>
